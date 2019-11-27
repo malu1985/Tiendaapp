25d5f5e657f8e413db9954b15c8f04bf94c9fbc6
malu1985 <56706773+malu1985@users.noreply.github.com>
2019-11-26 22:47:00 -0500

    Update index.php

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
 		<link rel="shortcut icon" href="../assets/img/favicon.png" />
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+		<script src="https://code.jquery.com/jquery-3.4.1.js">
+</script>
 
    
 			
@@ -66,8 +68,7 @@
 			</div>
 		</div>
 	</body>
-<script src="https://code.jquery.com/jquery-3.4.1.js">
-</script>
+
 
 <script>
 $(document).ready(function(){
