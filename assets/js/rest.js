327028a617411f0df040f139c38b9350b8c98000
malu1985 <56706773+malu1985@users.noreply.github.com>
2019-11-26 21:14:14 -0500

    Add files via upload

---
+++ assets/js/rest.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+$.ajax({
+
+url:"https://maryluz.herokuapp.com/public/index.php/api/customers
+}).then(function(data)){
+$('.greeting-id').appen(data.id);
+$('.greeting-content').appen(data.first_name);
+
+
+});
+
+});
