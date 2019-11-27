94dc2d81abf8c6abc4150e43a93490ef9f6c932f
malu1985 <56706773+malu1985@users.noreply.github.com>
2019-11-26 23:00:10 -0500

    Update index.php

--- index.php
+++ index.php
@@ -74,8 +74,8 @@ $(document).ready(function(){
 $.ajax({
 url:"https://maryluz.herokuapp.com/public/index.php/api/customers"
 }).then(function(data){
-$('.greeting-id').appen(data.id);
-$('.greeting-content').appen(data.first_name);
+$('.greeting-id').appenChild(data.id);
+$('.greeting-content').appenChild(data.first_name);
 console.log(data);
 });
 });
