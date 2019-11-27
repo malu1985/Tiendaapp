2b30af31c154f29df824a1d7dc989b7c2e60722c
malu1985 <56706773+malu1985@users.noreply.github.com>
2019-11-27 08:22:07 -0500

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 		<link rel="stylesheet" type="text/css" href="framework/css/bootstrap.css">
 		<link rel="stylesheet" type="text/css" href="css/bootstrap.css">
-<script src="assets//jquery/jquery.min.js"></script>
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 
    
 			
