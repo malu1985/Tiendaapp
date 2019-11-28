a0b0429f00053aa11ff9cb518b2220245a0a2689
malu1985 <56706773+malu1985@users.noreply.github.com>
2019-11-28 08:45:05 -0500

    Update rest.js

--- assets/js/rest.js
+++ assets/js/rest.js
@@ -4,6 +4,6 @@ url:"https://maryluz.herokuapp.com/public/index.php/api/customers"
 }).then(function(data){
 $(".greeting-id").append(data.id);
 $(".greeting-first_name").append(data.first_name);
-alert((data.id));
+alert((data));
 });
 });
