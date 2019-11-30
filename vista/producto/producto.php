8e60fc4b8ef69f9eafb50114b2e11e6ad070405a
malu1985 <56706773+malu1985@users.noreply.github.com>
2019-11-29 23:58:11 -0500

    Update producto.php

--- vista/producto/producto.php
+++ vista/producto/producto.php
@@ -1,5 +1,5 @@
 <h1 class="page-header">Productos </h1>
-<li  class="video-list table-list active" data-name="Search"></li>
+<p>Id Activos><li  class="video-list table-list active"></li></p>
 
 
     <a class="btn btn-primary pull-right" href="?c=producto&a=Crud">Agregar</a>
