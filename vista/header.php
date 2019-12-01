fcfa898d53735b98c6132150e3187c86d593d6db
malu1985 <56706773+malu1985@users.noreply.github.com>
2019-12-01 17:32:51 -0500

    Update header.php

--- vista/header.php
+++ vista/header.php
@@ -26,7 +26,6 @@ echo"<script type='text/javascript'>
         <link rel="stylesheet" href="../assets/js/jquery-ui/jquery-ui.min.css" />
         <link rel="stylesheet" href="../assets/css/style.css" />
         <link href="..assets/plugins/datatables/dataTables.bootstrap.css" rel="stylesheet" type="text/css" />
-        <script src="http://code.jquery.com/jquery-1.11.2.min.js"></script>
         <script src="../assets/plugins/datatables/jquery.dataTables.js" type="text/javascript"></script>
     	<script src="../assets/plugins/datatables/dataTables.bootstrap.js" type="text/javascript"></script>
         <script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
