9e49971f1a93d30a6fc075593a8fa87e59cbf053
malu1985 <56706773+malu1985@users.noreply.github.com>
2019-12-01 15:37:26 -0500

    Update registro_vista.php

--- vista/registro_vista.php
+++ vista/registro_vista.php
@@ -13,16 +13,17 @@
 	</head>
 
 	<body>
-		 <div class="container">
-			<div class="col-sm-10" style="width:600px; margin-left: 250px;margin-top: 50px">
+		 <div align="center"class="container">
+			<div  style="width:600px; margin-top: 5%">
 				
 				<div class="jumbotron boxlogin">
-					<div class="form-group" style="margin-top:-50px;">
-							<h1 style="margin-left: 150px;">REGISTRO</h1>
+					<div class="form-group" style="margin-top:-30px;">
+							<h1 style="margin-left: 90px;">REGISTRO</h1>
 					</div>
 					<hr>
 					</hr>
 					<form class="form-horizontal" method="POST" name="flogin" id="flogin"  action="../controlador/registro_controlador.php" style="margin-left: 50px; ">
+					<div align="left">
 						<label>Ingrese Cedula:</label>
 							<div class="form-group input-group">
 								<span class="input-group-addon">
@@ -30,6 +31,8 @@
 								</span>
 								<input type="text" name="cedula" id="cedula" class="form-control" placeholder="ingresa cedula" required pattern="[0-9]{1,40}" title="Solo se aceptan numeros">
 							</div>
+							</div>
+							<div align="left">
 						<label>Nombre de Usuario:</label>
 							<div class="form-group input-group">
 								<span class="input-group-addon">
@@ -37,6 +40,8 @@
 								</span>
 								<input type="text" name="username" id="username" class="form-control" placeholder="ingresa usuario" required>
 							</div>
+							</div>
+							<div align="left">
 								<label>Apellido usuario:</label>
 							<div class="form-group input-group">
 								<span class="input-group-addon">
@@ -44,6 +49,8 @@
 								</span>
 								<input type="text" name="subname" id="subname" class="form-control" placeholder="ingresa apellido" required pattern="[A-Za-z]{1,20}" title="Solo se aceptan letras">
 							</div>	
+							</div>
+							<div align="left">
 								<label>Correo usuario:</label>
 							<div class="form-group input-group">
 								<span class="input-group-addon">
@@ -51,7 +58,8 @@
 								</span>
 								<input type="email" name="correo" id="correo" class="form-control" placeholder="ingresa correo" required>
 							</div>
-							
+							</div>
+							<div align="left">
 								<label>Contraseña:</label>
 							<div class="form-group input-group">
 								<span class="input-group-addon">
@@ -59,13 +67,13 @@
 								</span>
 								<input type="password" name="password" id="password" class="form-control" placeholder="ingresa contraseña" required>
 							</div>
-							
-								
+							</div>
 							<div class="form-group">
 								<input type="submit" class="btn btn-success" value="Registrar" style="width:430px;">
 							</div>
-						<div class="form-group">
-							<a href="../index.php" style="margin-left: 150px;">INICIAR SESION</a>
+							
+						<div align="left" class="form-group">
+							<a href="..index.php" >Iniciar Sesión</a>
 						</div>
 					</form>
 				</div>
