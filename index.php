f5ab79d695e412cefc2ed9dbe600e1e7ecf83423
malu1985 <56706773+malu1985@users.noreply.github.com>
2019-12-01 18:51:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 	<head>
 		<title>Login | Sistema de Gestión Tiendapp</title>
 		
-		<meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=no;" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 		<script src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
