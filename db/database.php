a32fb76050e6828cec8dbba94b1a6ebfb6c3df20
malu1985 <56706773+malu1985@users.noreply.github.com>
2020-01-14 11:27:05 -0500

    Update database.php

--- db/database.php
+++ db/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function StartUp()
     {
-        $pdo = new PDO('mysql:host=remotemysql.com;dbname=KC7tralXh6;charset=utf8', 'KC7tralXh6', 'QcvHIFQPRV');
+        $pdo = new PDO('mysql:host=remotemysql.com;dbname=tK42tdZGro;charset=utf8', 'tK42tdZGro', '0bhEbygdy6');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
@@ -11,7 +11,7 @@ class Database
 
 class Conectar{
     public static function conexion(){
-        $conexion=new mysqli("remotemysql.com", "KC7tralXh6", "QcvHIFQPRV", "KC7tralXh6");
+        $conexion=new mysqli("remotemysql.com", "tK42tdZGro", "0bhEbygdy6", "tK42tdZGro");
         $conexion->query("SET NAMES 'utf8'");
         return $conexion;
     }
