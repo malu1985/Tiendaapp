ac4024a722381f1e89e08cdb6e031cac72a9f79f
malu1985 <56706773+malu1985@users.noreply.github.com>
2020-05-27 21:27:37 -0500

    Update producto_user.php

--- vista/producto/producto_user.php
+++ vista/producto/producto_user.php
@@ -5,19 +5,27 @@
 <br><br><br>
 
 <table class="table  table-striped  table-hover" id="tabla">
-    <thead>
+   <thead>
         <tr>
-		<h3>Busca cliente</h3>
-        <input class="form" value="">
-		<h3>Fecha</h3>
-		<input class="form" value="">
-		<a href="vista_productouser.php"><span class="glyphicon glyphicon-search"></span></a>
+        <th style="width:120px; background-color: #5DACCD; color:#fff">Id</th>
+            <th style="width:180px; background-color: #5DACCD; color:#fff">Nombre</th>
+            <th style=" background-color: #5DACCD; color:#fff">precio</th>
+            <th style=" background-color: #5DACCD; color:#fff">stock</th>
+            <th style="width:120px; background-color: #5DACCD; color:#fff">id_categoria</th>            
+			<th style="width:120px; background-color: #5DACCD; color:#fff">id_proveedor</th>
+            <th style="width:60px; background-color: #5DACCD; color:#fff"></th>
+            <th style="width:60px; background-color: #5DACCD; color:#fff"></th>
         </tr>
     </thead>
     <tbody>
     <?php foreach($this->model->Listar() as $r): ?>
         <tr>
-		
+	<td><?php echo $r->id_producto; ?></td>
+         <td><?php echo $r->nombre; ?></td>
+            <td><?php echo $r->precio; ?></td>
+            <td><?php echo $r->stock; ?></td>
+            <td><?php echo $r->id_categoria; ?></td>
+            <td><?php echo $r->id_proveedor; ?></td>	
 
         </tr>
     <?php endforeach; ?>
